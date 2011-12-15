682351e3cf3fb0c07748fc826e703a79f36f68e1
Felix Hoerandner <felix.hoerandner@gmx.at>
2011-12-15 16:20:59 +0100

    fixed whitespaces

--- pipeline.js
+++ pipeline.js
@@ -1,94 +1,94 @@
 (function(ext){
 
-	//----------------------------------------------------------------------------
-	var forEach = ext.forEach = function(collection, iterator, callback, context){
-		var executor = function(callback, context){
-			var results = [];
-			results.length = collection.length;
-			var errors = [];
+  //----------------------------------------------------------------------------
+  var forEach = ext.forEach = function(collection, iterator, callback, context){
+    var executor = function(callback, context){
+      var results = [];
+      results.length = collection.length;
+      var errors = [];
 
-			var setDone = function(index, err){
-				results[index] = true;
-				if(err !== undefined)
-					errors.push(err);
+      var setDone = function(index, err){
+        results[index] = true;
+        if(err !== undefined)
+          errors.push(err);
 
-				for(var i = 0; i < results.length; i++)
-					if(results[i] !== true)
-						return;
+        for(var i = 0; i < results.length; i++)
+          if(results[i] !== true)
+            return;
 
-				callback(errors.length === 0 ? undefined : errors);
-			};
+        callback(errors.length === 0 ? undefined : errors);
+      };
 
-			var generateSetDone = function(index){
-				return function(err){
-					setDone(index, err);
-				};
-			};
+      var generateSetDone = function(index){
+        return function(err){
+          setDone(index, err);
+        };
+      };
 
-			if(collection.length <= 0)
-				callback();
-			else
-				for(var i = 0; i < collection.length; i++)
-					try{
-						iterator.call(context, collection[i], generateSetDone(i));
-					}catch(err){
-						setDone(i, err);
-					}
-		};
-		
-		if(arguments.length === 2)
-			return executor;
-		else
-			executor(callback, context || this);
-	};
+      if(collection.length <= 0)
+        callback();
+      else
+        for(var i = 0; i < collection.length; i++)
+          try{
+            iterator.call(context, collection[i], generateSetDone(i));
+          }catch(err){
+            setDone(i, err);
+          }
+    };
 
-	//----------------------------------------------------------------------------
-	var parallel = ext.parallel = function(functions, callback, context){
-		var executor = function(callback, context){
-			context = context || this;
-			forEach(functions, function(func, done){
-				func.call(context, done);
-			}, callback, context);
-		};
+    if(arguments.length === 2)
+      return executor;
+    else
+      executor(callback, context || this);
+  };
 
-		if(arguments.length === 1)
-			return executor;
-		else
-			executor(callback, context || this);
-	};
+  //----------------------------------------------------------------------------
+  var parallel = ext.parallel = function(functions, callback, context){
+    var executor = function(callback, context){
+      context = context || this;
+      forEach(functions, function(func, done){
+        func.call(context, done);
+      }, callback, context);
+    };
 
-	//----------------------------------------------------------------------------
-	var sequence = ext.sequence = function(functions, callback, context){
-		var executor = function(callback, context){
-			context = context || this;
-			
-			var createCallback = function(index){
-				if(index < functions.length)
-					return function(err){
-						if(err){
-							callback(err);
-							return;
-						}
+    if(arguments.length === 1)
+      return executor;
+    else
+      executor(callback, context || this);
+  };
 
-						try{
-							functions[index].call(context, createCallback(index + 1));
-						}catch(err){
-							callback(err);
-						}
-					};
-				else
-					return function(err){
-						callback.apply(context, arguments);
-					};
-			};
+  //----------------------------------------------------------------------------
+  var sequence = ext.sequence = function(functions, callback, context){
+    var executor = function(callback, context){
+      context = context || this;
 
-			createCallback(0)();
-		};
+      var createCallback = function(index){
+        if(index < functions.length)
+          return function(err){
+            if(err){
+              callback(err);
+              return;
+            }
 
-		if(arguments.length === 1)
-			return executor;
-		else
-			executor(callback, context || this);
-	};
+            try{
+              functions[index].call(context, createCallback(index + 1));
+            }catch(err){
+              callback(err);
+            }
+          };
+        else
+          return function(err){
+            callback.apply(context, arguments);
+          };
+      };
+
+      createCallback(0)();
+    };
+
+    if(arguments.length === 1)
+      return executor;
+    else
+      executor(callback, context || this);
+  };
 
 })(typeof exports === 'undefined' ? (this.pipeline = {}) : exports);
